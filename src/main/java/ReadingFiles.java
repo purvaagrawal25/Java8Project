086a088b18b52e6f5e68ff7b6c286eb11b60b2cd
purvaagrawal25 <69458627+purvaagrawal25@users.noreply.github.com>
2020-10-16 12:03:50 +0530

    Update ReadingFiles.java

--- src/main/java/ReadingFiles.java
+++ src/main/java/ReadingFiles.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 public class ReadingFiles {
     public static void main(String[] args) {
-        String filePath = "/home/knoldus/IdeaProjects/Java8Project/src/main/resources/TextFile.txt";
+        String filePath = "/home/knoldus/IdeaProjects/Java8Project/src/main/TextFile.txt";
         try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
             Map<String, Long> frequencyOfEachWord = stream.flatMap(line -> Arrays.stream(line.split(" ")))
                     .map(String::toLowerCase)
